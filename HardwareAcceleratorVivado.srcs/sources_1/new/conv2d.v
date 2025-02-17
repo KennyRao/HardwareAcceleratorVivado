dad38c2db4870dde71839240aeba3ff768e96dc7
Weikai Rao <weikairao@gmail.com>
2025-02-17 13:39:44 -0600

    Init

---
+++ HardwareAcceleratorVivado.srcs/sources_1/new/conv2d.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/02/17 13:32:32
+// Design Name: 
+// Module Name: conv2d
+// Project Name: HardwareAcceleratorVivado
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module conv2d(
+
+    );
+endmodule
